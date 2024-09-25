f9d2317052d4c8a599fe99fd231f9f1016291971
NurAmanahZaen <nurmaaz06@gmail.com>
2024-09-25 11:04:37 +0700

    nurchoerunnisa menambahkan data barang

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,5 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/pelanggan/json', 'Pelanggan::showSimpleJson');
 $routes->get('/pelanggan/data-pelanggan', 'Pelanggan::getPelangganDataJson');
+$routes->get('/barang/json', 'Barang::showSimpleJson');
+$routes->get('/barang/data-barang', 'Barang::getBarangDataJson');
